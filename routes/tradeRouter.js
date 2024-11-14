74f01752c864169d3af3326d6291020dd66541ee
Achintya <achintyadu@gmail.com>
2024-11-14 19:21:08 +0530

    applied condition on SBI trade , rest of the trades are being accepted

--- routes/tradeRouter.js
+++ routes/tradeRouter.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const { seoHeadTagValues } = require("../utils/helpers");
 const { PAGE_NAME } = require("../utils/constants");
 const asyncMiddleware = require("../config/asyncMiddleware.config");
+const { error } = require("winston");
 
 let trades = [];
 
@@ -19,7 +20,8 @@ router.get("/add", (req, res) => {
     menu: "Journal",
     ...seoHeadTagValues(PAGE_NAME.HOME),
     trades,
-    message: null,
+    message:null,
+    error:null,
   });
 });
 
@@ -32,18 +34,25 @@ router.post("/add", (req, res) => {
   // localhost:5110/trade/add
   const { stockName, price, orderType } = req.body;
 
-  if (stockName && price && orderType) {
-    trades.push({ stockName, price, orderType });
+  if (stockName === "SBI") {
+    return res.render("trades/addTrade", {
+      menu: "Journal",
+      ...seoHeadTagValues(PAGE_NAME.HOME),
+      trades,
+      message: null,
+      error: "Cannot add stock", 
+    });
   }
 
   const newTrade = { stockName, price, orderType };
   trades.push(newTrade);
-  
+
   res.render("trades/addTrade", {
     menu: "Journal",
     ...seoHeadTagValues(PAGE_NAME.HOME),
     trades,
     message: "Trade added successfully",
+    error:null,
   });
   // res.status(201).json(newTrade);
 });
