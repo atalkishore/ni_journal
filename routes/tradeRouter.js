740dba62b39dc2f8f7d909273c6ffd9b1719b0bd
Achintya <achintyadu@gmail.com>
2024-11-14 16:20:36 +0530

    added show all trades button

--- routes/tradeRouter.js
+++ routes/tradeRouter.js
@@ -4,11 +4,7 @@ const { seoHeadTagValues } = require("../utils/helpers");
 const { PAGE_NAME } = require("../utils/constants");
 const asyncMiddleware = require("../config/asyncMiddleware.config");
 
-let trades = [
-  { stockName: "SBIN", orderType: "MARKET ORDER", price: "550" },
-  { stockName: "TATA", orderType: "LIMIT ORDER", price: "500" },
-  { stockName: "SUZLON", orderType: "STOP ORDER", price: "50" },
-];
+let trades = [];
 
 router.get("/", (req, res) => {
   res.render("trades/trades", {
@@ -18,28 +14,38 @@ router.get("/", (req, res) => {
   });
 });
 
-router.get("/", (req, res) => {
+router.get("/add", (req, res) => {
   res.render("trades/addTrade", {
     menu: "Journal",
     ...seoHeadTagValues(PAGE_NAME.HOME),
+    trades,
+    message: null,
   });
 });
 
 router.get("/api/trades", (req, res) => {
+  // localhost:5110/trade/api/trades
   res.json(trades);
 });
 
-router.post("/api/trades", (req, res) => {
+router.post("/add", (req, res) => {
+  // localhost:5110/trade/add
   const { stockName, price, orderType } = req.body;
 
-  if (!stockName || !price || !["buy", "sell"].includes(orderType)) {
-    return res.status(400).json({ error: "Invalid trade data" });
+  if (stockName && price && orderType) {
+    trades.push({ stockName, price, orderType });
   }
 
   const newTrade = { stockName, price, orderType };
   trades.push(newTrade);
-
-  res.status(201).json(newTrade);
+  
+  res.render("trades/addTrade", {
+    menu: "Journal",
+    ...seoHeadTagValues(PAGE_NAME.HOME),
+    trades,
+    message: "Trade added successfully",
+  });
+  // res.status(201).json(newTrade);
 });
 
 module.exports = router;
