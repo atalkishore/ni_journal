946e1acc2edec38e3c4e36c741e0df764bdfd263
Atal Kishore <atalkishore@gmail.com>
2024-12-24 22:54:02 +0530

    comm fn

--- public/journal/js/common-fn.js
+++ public/journal/js/common-fn.js
@@ -25,11 +25,7 @@ $(document).ready(function () {
         myModal.hide();
       }, 500);
     },
-    success: function (data, textStatus, jqXHR) {
-      // Optionally handle success
-    },
     error: function (jqXHR, textStatus, errorThrown) {
-      console.error('AJAX Request Failed:', textStatus, errorThrown); // Log the error
       setTimeout(() => {
         myModal.hide();
       }, 500);
@@ -86,24 +82,3 @@ const disableRapidClick = (button, time = 3) => {
   button.disabled = true;
   time && setTimeout(() => (button.disabled = false), time * 1000);
 };
-
-$(document).ready(function () {
-  // Function to update visibility based on checkbox state
-  function updateVisibility() {
-    if (!$('#oc-perlot-toggle').is(':checked')) {
-      // If checked, show 'oc_disp_inlot' and hide 'oc_disp_inQty'
-      $('.oc_disp_inlot').show(); // Show oc_disp_inlot
-      $('.oc_disp_inQty').hide(); // Hide oc_disp_inQty
-    } else {
-      // If unchecked, hide 'oc_disp_inlot' and show 'oc_disp_inQty'
-      $('.oc_disp_inlot').hide(); // Hide oc_disp_inlot
-      $('.oc_disp_inQty').show(); // Show oc_disp_inQty
-    }
-  }
-
-  // Initial visibility setup
-  updateVisibility();
-
-  // Event listener for checkbox toggle
-  $('#oc-perlot-toggle').change(updateVisibility);
-});
